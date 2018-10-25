b0b7531752d39bc56fd11bf21430a3ce23e1b518
Alexander Hardt <alexanderhardt@Alexanders-MacBook-Pro.local>
2018-10-25 16:07:10 -0700

    ping if else branch works

--- cs4558_project_CZA_script1.sh
+++ cs4558_project_CZA_script1.sh
@@ -1,3 +1,8 @@
+
+
+NUM_PINGS=5
+# urls_to_ping = []
+
 #specs:
 	#desired output:
 		# 1. send several pings
@@ -7,12 +12,34 @@
 #1.find default gateway 
 DEF_GATE_WAY_STRING="$(route get default | grep gateway | cut -d':' -f 2)"
 # echo"x"
-echo "$DEF_GATE_WAY_STRING"
+#echo "$DEF_GATE_WAY_STRING"
 
-ping $DEF_GATE_WAY_STRING
+HOST=$DEF_GATE_WAY_STRING
 
-#stop pings
+ping -c1 $HOST #1>/dev/null 2>/dev/null
+SUCCESS=$?
 
+if [ $SUCCESS -eq 0 ]
+then
+  echo "$HOST has replied"
+else
+  echo "$HOST didn't reply"
+fi
+#EOF
 
+# check for success of gateway ping
+	# if ping gateway success, 
+		# ping list of hosts
+			#use average of 2-5 pings 
+				#ping -c 5 url
+			#write output of each ping to file
+				#specifically 
+					# TOD (time of day)
+					# HOST (Gateway or IP_ADDR from urls_to_ping)
+					# ping stats (average roundtrip time of pings)
+				# how to write
+					# echo "statss" >> ping_file_start_date_time
+	# else (no ping success)
+		#insert FAILED_TO_PING_GATEWAY into PING_FILE.txt
 
-#2 record data received
+	# sleep 10 mins
