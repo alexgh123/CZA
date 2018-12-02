5ae578fc7b9957cd95c5b7c2ab6ef3b9c50b1bc4
Alexander Hardt <alexanderhardt@Alexanders-MacBook-Pro.local>
2018-12-01 17:39:06 -0800

    can iterate over all files

--- analysis_script.py
+++ analysis_script.py
@@ -47,55 +47,86 @@
 import csv
 from datetime import datetime
 
-counter=0
-limit_to_lines = 25000
-second_counter = 0
-dict_of_sites = {}
 
-num_bins = 2
+# num_bins = 2
 seconds_in_an_hour = 3600
+# num_failed_recs = 0
 
-def Average(list): 
-    return sum(list) / len(list) 
+def Average(list):
+	if((len(list))==0):
+		# print("here is bad list:")
+		# print(list)
+		output = "no_pings"
+	else:
+		output = sum(list) / len(list) 
+	return output
 
-with open('20181126_A_outFile.txt') as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter=',')
 
-    for line in csv_reader:
-		counter += 1
-		#don't use first three lines, its metadata
-		if(counter<= 3):
-			continue
-		#break at limit for development purposes
-		if (counter >= limit_to_lines):
-			break
-		
-		# need to get seconds since midnight for each row
-		# so that I know which bin to put each rec in
-		# data format 26/11/2018 00:00:05 UTC
-		row_time = datetime.strptime(line[3], ' %d/%m/%Y %H:%M:%S %Z ')
-		seconds_since_midnight = (row_time - row_time.replace(hour=0, minute=0, second=0, microsecond=0)).total_seconds()
-		
-		# below operation will provide correct index number to find correct 
-		# hourly bin to store times in
-		correct_bin_index = int(seconds_since_midnight/seconds_in_an_hour)
-		
-		#format some data...
-		url = line[1].strip()
-		ping_time = float(line[2].strip())
+def analyzeFile(fileName):
 
-		#do dictionary check, if key in dict...
-		# just add the record
-		if(url in dict_of_sites):
-			dict_of_sites[url][correct_bin_index].append(ping_time)
-		#if key doesn't exist, create blank bins
-		# add first time to correct bin
-		else:
-			bins = [[] for _ in range(24)]
-			# print("dict doesn't have key, here is line[1]")
-			# print(url)
-			dict_of_sites[url]= bins
-			dict_of_sites[url][correct_bin_index].append(ping_time)
+	counter=0
+	num_failed_recs = 0
+	# limit_to_lines = 250
+	dict_of_sites = {}
+
+	with open(fileName) as csv_file:
+	    csv_reader = csv.reader(csv_file, delimiter=',')
+	
+	    for line in csv_reader:
+			counter += 1
+			#don't use first three lines, its metadata
+			if(counter<= 3):
+				continue
+			#break at limit for development purposes
+			#if (counter >= limit_to_lines):
+			#	break
+			
+			# need to get seconds since midnight for each row
+			# so that I know which bin to put each rec in
+			# data format 26/11/2018 00:00:05 UTC
+			# some data is corrupted, so if below line fails, 
+			# it means we don't have average ping time for it
+			try:
+				row_time = datetime.strptime(line[3], ' %d/%m/%Y %H:%M:%S %Z ')
+			except:
+				#if the above operation fails, we'll throw out the data
+				num_failed_recs +=1
+				# print("failed with this line:")
+				# print(line)
+				continue
+			seconds_since_midnight = (row_time - row_time.replace(hour=0, minute=0, second=0, microsecond=0)).total_seconds()
+			
+			# below operation will provide correct index number to find correct 
+			# hourly bin to store times in
+			correct_bin_index = int(seconds_since_midnight/seconds_in_an_hour)
+			
+			#format some data...
+			url = line[1].strip()
+			try:
+				ping_time = float(line[2].strip())
+			except:
+				num_failed_recs +=1
+				continue
+
+	
+			#do dictionary check, if key in dict...
+			# just add the record
+			if(url in dict_of_sites):
+				dict_of_sites[url][correct_bin_index].append(ping_time)
+			#if key doesn't exist, create blank bins
+			# add first time to correct bin
+			else:
+				bins = [[] for _ in range(24)]
+				# print("dict doesn't have key, here is line[1]")
+				# print(url)
+				dict_of_sites[url]= bins
+				dict_of_sites[url][correct_bin_index].append(ping_time)
+
+	print("done analyzing this file:")
+	print(fileName)
+	print('this many exceptions:')
+	print(num_failed_recs)
+	return dict_of_sites
 
 #at this point, we have the dictionary object
 #    {url:[[],[]...]}
@@ -103,27 +134,50 @@ with open('20181126_A_outFile.txt') as csv_file:
 #BUT we need average ping time for each hour!
 # (below code does that)
 
-new_dict = dict_of_sites
-for url in new_dict:
-	index_into_nested_list = 0
-	for hourly_bin in new_dict[url]:
-		hourly_bin = Average(hourly_bin)
-		#below line introduces dependency on our input file (specifically index number)
-		# but its ok because our log files run all day, and midnight is the first
-		# hour in the day
-		new_dict[url][index_into_nested_list] = round(hourly_bin, 3)
-		index_into_nested_list += 1
+def getAverageHourlyPing(dictInput):
+	for url in dictInput:
+		index_into_nested_list = 0
+		for hourly_bin in dictInput[url]:
+			# if(len(hour))
+			hourly_bin = Average(hourly_bin)
+			#below line introduces dependency on our input file (specifically index number)
+			# but its ok because our log files run all day, and midnight is the first
+			# hour in the day
+			if(hourly_bin=="no_pings"):
+				avg = "no_pings"
+			else:
+				avg = round(hourly_bin, 3)
+			dictInput[url][index_into_nested_list] = avg
+			index_into_nested_list += 1
+	return dictInput 
+
+# file = "20181129_Z_outFile.txt"
+# new_dict = analyzeFile(file)
+# hourlyAverage = getAverageHourlyPing(new_dict)
 
 # print("here is your dictionary of average ping time, hourly...")
-# print(new_dict)
+# print(hourlyAverage)
 
+# exit()
 
-#write output somewhere productive:
-	#googleFile
-	#yahooFile
-		# output to those files:
-			# date:avg_for_that_day
+#get list of files
+#iterate through them
+with open("list_of_files.txt") as f:
+    listOfFiles = f.readlines()
+listOfFiles = [x.strip() for x in listOfFiles] 
+# print(listOfFiles)
 
+# exit()
+
+for file in listOfFiles:
+	new_dict = analyzeFile(file)
+	hourlyAverage = getAverageHourlyPing(new_dict)
+	print("this file:")
+	print(file)
+	print("generates this data:")
+	print(hourlyAverage)
+	print(" ")
+	print(" ")
 
 
 
